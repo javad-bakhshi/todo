28f47684fa6809272ecef56e09c227741ba20c9b
unknown <javad.bakhshi@gmail.com>
2019-12-13 19:10:35 +0330

    completed

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -5,6 +5,11 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('about/',views.about,name='about'),
-    path('delete/<list_id>',views.delete,name='delete')
+    path('delete/<list_id>',views.delete,name='delete'),
+    path('uncross/<list_id>',views.uncross,name='uncross'),
+    path('cross_off/<list_id>',views.cross_off,name='cross_off'),
+    path('edit/<list_id>',views.edit,name='edit'),
+
+
 
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -3,6 +3,7 @@ from .models import List
 from .forms import ListForm
 from django.contrib import messages
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
@@ -17,7 +18,8 @@ def home(request):
 			all_items=List.objects.all
 			messages.success(request,('Item Has Been Added To list!'))
 			context={'all_items':all_items}
-			return render(request, 'home.html', context)
+			return HttpResponseRedirect(reverse('home'))
+			#return render(request, 'home.html', context)
 
 	
 	else:
@@ -33,3 +35,30 @@ def delete(request,list_id):
 	item.delete()
 	messages.success(request,('Item Has Been Deleted!'))
 	return redirect('home')
+
+def cross_off(request,list_id):
+	item=List.objects.get(pk=list_id)
+	item.compeleted=True
+	item.save()
+	return redirect('home')
+
+def uncross(request,list_id):
+	item=List.objects.get(pk=list_id)
+	item.compeleted=False
+	item.save()
+	return redirect('home')
+
+def edit(request,list_id):
+	if request.method=="POST":
+		item=List.objects.get(pk=list_id)
+		form=ListForm(request.POST or None,instance=item)
+
+		if form.is_valid():
+			form.save()
+			messages.success(request,"Item Edited")
+			return redirect('home')
+
+	else:
+	    item=List.objects.get(pk=list_id)
+	    context={'item':item}
+	    return render(request, 'edit.html', context)
